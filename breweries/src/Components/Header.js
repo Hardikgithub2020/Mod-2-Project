168d26deba698825aff1bd45db0343879cd8b08e
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-16 14:04:01 -0400

    Add another functional component

---
+++ breweries/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div className='main-Header'>
+        <h1>BREWERIES </h1>
+            
+        </div>
+    );
+}
+
+export default Header;
