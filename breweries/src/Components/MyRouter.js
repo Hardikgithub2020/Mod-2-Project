9e7acdebe6a8c403072b3d6c9ea68ade9083d5f4
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-16 13:34:14 -0400

    Creat Router

---
+++ breweries/src/Components/MyRouter.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import State from "./State";
+import UnitedState from "./UnitedState";
+
+function MyRouter(props) {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/UnitedState">UnitedState</Link>
+            </li>
+            <li>
+              <Link to="/State">State</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/UnitedState">
+            <UnitedState />
+          </Route>
+          <Route path="/State">
+            <State />
+          </Route>
+          <Route path="/">{/* <Home /> */}</Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default MyRouter;
