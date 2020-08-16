e3f529db057ef793fd939a1579794ebeac3de9a2
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-15 23:59:54 -0400

    Create reducer in index.js

--- breweries/src/index.js
+++ breweries/src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import {createStore} from 'redux'
 
+const initialData = { 
+  data : []
+};
+const reducer = (state=initialData,action)=>{
+ switch(action.type){
+   case 'success' : return ({...state, data : action.payload});
+   default :  return state;
+ }
+}
+const store=createStore(reducer);
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
