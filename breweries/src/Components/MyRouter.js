ce502d5a1d4bec3af0b352196df133a959dd6074
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-17 15:21:59 -0400

    Made code more cleaner

--- breweries/src/Components/MyRouter.js
+++ breweries/src/Components/MyRouter.js
@@ -7,33 +7,14 @@ import UnitedState from "./UnitedState";
 function MyRouter(props) {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul type='none' style={{display:"flex"}}>
-            <li>
-              <Link to="/">Home |</Link>
-            </li>
-            <li>
-              <Link to="/UnitedState">United State   |</Link>
-            </li>
-            <li>
-              <Link to="/State">State |</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-                renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/UnitedState">
-            <UnitedState />
-          </Route>
-          <Route path="/State">
-            <State />
-          </Route>
-          <Route path="/">{/* <Home /> */}</Route>
-        </Switch>
-      </div>
+      <Link to="/">Home |</Link>
+      <Link to="/UnitedState">United State |</Link>
+      <Link to="/State">State |</Link>
+      <Switch>
+        <Route path="/UnitedState" component={UnitedState} />
+        <Route path="/State" component={State} />
+        <Route path="/" />
+      </Switch>
     </Router>
   );
 }
