ce45698c8614bf700762265dcd51ede3eb7607d7
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-17 08:08:45 -0400

    add Search field

--- breweries/src/Components/State.js
+++ breweries/src/Components/State.js
@@ -3,19 +3,37 @@ import Page from './Page';
 import {connect} from 'react-redux'
 
 class State extends Component {
-    componentWillMount() {
-        fetch("https://api.openbrewerydb.org/breweries?by_state=ohio")
+    constructor(props){
+        super(props)
+        this.state={
+            stateName : 'ohio'
+        }
+    }
+    UNSAFE_componentWillMount() {
+        fetch(`https://api.openbrewerydb.org/breweries?by_state=${this.state.stateName}`)
           .then((res) => res.json())
           .then((json) => {
             this.props.dispatch({ type: "success", payload: json });
           })
           .catch((error) => this.props.dispatch(error));
       }
-    
+    handlesubmit=(e)=> {
+            fetch("https://api.openbrewerydb.org/breweries?by_state=${e.target.value}")
+              .then((res) => res.json())
+              .then((json) => {
+                this.props.dispatch({ type: "success", payload: json });
+              })
+              .catch((error) => this.props.dispatch(error));
+          }
+    // Hoe can i use user input for a serach
     render() {
         return (
             <div>
-                <h1>Breweries in Ohio</h1>
+                <h1>Search Breweries by state </h1>
+                <form obSubmit={this.handlesubmit}>
+                    <input name='stateName' type='text' value={this.state.stateName} onChange={(event)=>this.setState({stateName : event.target.value})} />
+                    <input type='submit' value='search'/>
+                </form>
                 <Page/>
             </div>
         );
--- breweries/src/Components/UnitedState.js
+++ breweries/src/Components/UnitedState.js
@@ -4,13 +4,15 @@ import Page from "./Page"
 
 class UnitedState extends React.Component {
   componentWillMount() {
-    fetch("https://api.openbrewerydb.org/breweries/search?query=united_state")
+    fetch("https://api.openbrewerydb.org/breweries/search?query=united_state&sort=-state,-city,-type,-name")
       .then((res) => res.json())
       .then((json) => {
         this.props.dispatch({ type: "success", payload: json });
       })
       .catch((error) => this.props.dispatch(error));
   }
+
+  
 
   render() {
     
