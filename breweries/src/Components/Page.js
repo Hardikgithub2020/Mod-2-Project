f48e49736432dffe91824bb1d024294d4400e035
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-17 08:11:05 -0400

    Collect All State name in array without Duplicate Entry

--- breweries/src/Components/Page.js
+++ breweries/src/Components/Page.js
@@ -1,32 +1,40 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
 function Page(props) {
-    const { data } = props;
-    console.log("data", data);
-    const listInHTML = data.map((brewery, i) => {
-      return (
-        <div key={i} className='list'>
-          <h1 style={{ color: "greenyellow" }}> {brewery.name} </h1>
-          <p> Id : {brewery.id} </p>
-          <p> City : {brewery.city} </p>
-          <p> Country : {brewery.country} </p>
-          <p> Phone: {brewery.phone} </p>
-          <p> State : {brewery.state} </p>
-          <p> Street : {brewery.street} </p>
-          <a href={brewery.website_url} style={{ color: "khaki" }}>
-            {" "}
-            Website : {brewery.website_url}{" "}
-          </a>
-        </div>
-      );
-    });
+  const { data } = props;
+  // console.log("data", data);
+
+  // const listOfState = data.map((brewery) => brewery.state);
+
+  // const stateList = listOfState.filter(e=> e!=null);
+  const listOfState = [...new Set(data.map((brewery) => brewery.state))]; // Method used to remove allduplicate entries
+  const listOfCity = [...new Set(data.map((brewery) => brewery.city))]; 
+  const listOfBreweryType = [...new Set(data.map((brewery) => brewery.brewery_type))]; 
+  console.log(listOfState,listOfCity,listOfBreweryType);
+
+  // console.log('listOfstate',listOfState);
+
+  const listInHTML = data.map((brewery, i) => {
     return (
-        <div>
-            {listInHTML}
-        </div>
+      <div key={i} className="list">
+        <h1 style={{ color: "greenyellow" }}> {brewery.name} </h1>
+        <p> Id : {brewery.id} </p>
+        <p>Type: {brewery.brewery_type}</p>
+        <p> City : {brewery.city} </p>
+        <p> Country : {brewery.country} </p>
+        <p> Phone: {brewery.phone} </p>
+        <p> State : {brewery.state} </p>
+        <p> Street : {brewery.street} </p>
+        <a href={brewery.website_url} style={{ color: "khaki" }}>
+          {" "}
+          Website : {brewery.website_url}{" "}
+        </a>
+      </div>
     );
+  });
+  return <div>{listInHTML}</div>;
 }
-    
-const mapStateToProps= state => state;
+
+const mapStateToProps = (state) => state;
 export default connect(mapStateToProps)(Page);
