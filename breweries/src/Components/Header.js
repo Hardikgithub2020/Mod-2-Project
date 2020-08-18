6075080c1e67c5bc33cc4d7617828cd2fba99523
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-18 14:11:17 -0400

    Change image diplay position

--- breweries/src/Components/Header.js
+++ breweries/src/Components/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import MyRouter from "./MyRouter";
 import CallUseEffect from './CallUseEffect';
+import Footer from './Footer';
 
 function Header(props) {
     return (
         <div className='main-Header'>
         <h1 className='main-Title'>BREWERIES </h1>
-        <MyRouter />
         
+        <MyRouter />
+        <Footer/>
+       
         </div>
     );
 }
