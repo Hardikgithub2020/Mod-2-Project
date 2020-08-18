3e32f8f6804bb373336ed08021819cfdc7159558
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-18 14:03:34 -0400

    Change gif in home page

--- breweries/src/Components/Footer.js
+++ breweries/src/Components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Footer(props) {
     return (
-        <div>
-            <img src="https://www.verdict.co.uk/wp-content/uploads/2017/01/tumblr_nsijgpoxX01ur7mdeo1_500.gif"/>
+        <div className="footer">
+            <img src="https://mitendicotthouse.org/wp-content/uploads/2016/01/whiskey.gif"/>
         </div>
     );
 }
