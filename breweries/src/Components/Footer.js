1cb72316c8571f9e16528d3b617aad6534b0d3de
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-18 13:52:33 -0400

    Add Functional component Footer

---
+++ breweries/src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <div>
+            <img src="https://www.verdict.co.uk/wp-content/uploads/2017/01/tumblr_nsijgpoxX01ur7mdeo1_500.gif"/>
+        </div>
+    );
+}
+
+export default Footer;
