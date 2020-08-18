77497a2a30e20c7e61104d55dacc0137ac5bfb93
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-18 12:06:58 -0400

    Remove CallUseEffect from Header.js

--- breweries/src/Components/Header.js
+++ breweries/src/Components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import MyRouter from "./MyRouter";
+import CallUseEffect from './CallUseEffect';
 
 function Header(props) {
     return (
         <div className='main-Header'>
         <h1 className='main-Title'>BREWERIES </h1>
         <MyRouter />
+        
         </div>
     );
 }
