5bfce1dfa9a3200a553761d0c29bb75ff6d0bc8c
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-19 03:24:30 -0400

    Clear code by removing unwanted import

--- breweries/src/Components/Header.js
+++ breweries/src/Components/Header.js
@@ -1,20 +1,16 @@
-import React from 'react';
+import React from "react";
 import MyRouter from "./MyRouter";
-import CallUseEffect from './CallUseEffect';
-import Footer from './Footer';
+import Footer from "./Footer";
 
 function Header(props) {
-    return (
-        <div className='main-Header'>
-            {/* <Footer/> */}
-        <Footer/>
-        <h1 className='main-Title'>BREWERIES </h1>
-        
-        <MyRouter />
-       
-       
-        </div>
-    );
+  return (
+    <div className="main-Header">
+      {/* <Footer/> */}
+      <Footer />
+      <h1 className="main-Title">BREWERIES </h1>
+      <MyRouter />
+    </div>
+  );
 }
 
 export default Header;
