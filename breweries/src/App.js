7c27a1b2a98296b7f1de98449f3cbe653954f13b
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-19 03:17:06 -0400

    Update css

--- breweries/src/App.js
+++ breweries/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import User from "./Components/User"
 
 function App() {
   return (
     <div className="App">
+      <User/>
       <Header />
     </div>
   );
