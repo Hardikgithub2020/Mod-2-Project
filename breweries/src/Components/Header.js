b8c64e669f5c796494129bcd56f366ed87f1bb54
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-19 08:11:15 -0400

    Put user component in header

--- breweries/src/Components/Header.js
+++ breweries/src/Components/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import MyRouter from "./MyRouter";
 import Footer from "./Footer";
+import User from "./User";
 
 function Header(props) {
   return (
     <div className="main-Header">
       {/* <Footer/> */}
       <Footer />
+      <User/>
       <h1 className="main-Title">BREWERIES </h1>
       <MyRouter />
     </div>
