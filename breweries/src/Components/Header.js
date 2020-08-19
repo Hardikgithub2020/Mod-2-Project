0e734fc56d616a7ecbdc457220ac3cac9fba634d
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-19 00:42:59 -0400

    keep two image on background

--- breweries/src/Components/Header.js
+++ breweries/src/Components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import MyRouter from "./MyRouter";
 import CallUseEffect from './CallUseEffect';
-import Footer from './Footer';
+// import Footer from './Footer';
 
 function Header(props) {
     return (
         <div className='main-Header'>
-            <Footer/>
+            {/* <Footer/> */}
         <h1 className='main-Title'>BREWERIES </h1>
         
         <MyRouter />
---
+++ breweries/src/Components/UnitedStates.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { connect } from "react-redux";
+import Page from "./Page";
+import CallUseEffect from './CallUseEffect'
+
+
+
+class UnitedStates extends React.Component {
+  
+  // UNSAFE_componentWillMount() {
+  
+  //   fetch("https://api.openbrewerydb.org/breweries/search?query=united_state&sort=-state,-city,-type,-name")
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       this.props.dispatch({ type: "success", payload: json });
+  //     })
+  //     .catch((error) => this.props.dispatch(error));
+  // }
+
+  
+
+  render() {
+    // const { data } = this.props;
+    return (
+      // <div className='search-Bar'>
+      //   <h3>Breweries in UnitedState</h3>
+      //   <h3>No of Breweries : { data.length }</h3>
+      //   <Page/>
+      // </div>
+      <CallUseEffect/>
+     
+    );
+  }
+}
+export default UnitedStates;
+
+// const mapStateToProps = (state) => state;
+
+// export default connect(mapStateToProps)(UnitedStates);
