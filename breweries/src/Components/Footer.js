159e281745d6c94a44d301f7991c9ac119969025
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-19 09:20:24 -0400

    Add image in footer

--- breweries/src/Components/Footer.js
+++ breweries/src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(props) {
   return (
     <div className="footer">
-      {/* <img src="https://mitendicotthouse.org/wp-content/uploads/2016/01/whiskey.gif"/> */}
+      <img src="https://mitendicotthouse.org/wp-content/uploads/2016/01/whiskey.gif" style={{width :"98vw"}}/> 
       <img src="https://i.pinimg.com/originals/95/07/f2/9507f2f0a69e9a1795a2a08ebaa7fda7.gif" alt="gif" />
     </div>
   );
