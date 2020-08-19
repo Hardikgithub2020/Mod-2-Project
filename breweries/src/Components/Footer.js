08e8c549b075be233f0fb01ac12884608aaf54ff
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-19 01:22:21 -0400

    Reorganize CSS properties

--- breweries/src/Components/Footer.js
+++ breweries/src/Components/Footer.js
@@ -3,12 +3,13 @@ import Header from './Header'
 
 function Footer(props) {
     return (
-    
+        
         <div className="footer">
             {/* <img src="https://mitendicotthouse.org/wp-content/uploads/2016/01/whiskey.gif"/> */}
         <img src="https://i.pinimg.com/originals/95/07/f2/9507f2f0a69e9a1795a2a08ebaa7fda7.gif" />
-        
+       
         </div>
+
        
 
         
