2beed27d329ddba0a4eb6ae3a67d03a15b0fe9df
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-19 03:23:13 -0400

    Remove unnecessary dependency

--- breweries/src/Components/Footer.js
+++ breweries/src/Components/Footer.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import Header from './Header'
+import React from "react";
 
 function Footer(props) {
-    return (
-        
-        <div className="footer">
-            {/* <img src="https://mitendicotthouse.org/wp-content/uploads/2016/01/whiskey.gif"/> */}
-        <img src="https://i.pinimg.com/originals/95/07/f2/9507f2f0a69e9a1795a2a08ebaa7fda7.gif" />
-       
-        </div>
-
-       
-
-        
-    );
+  return (
+    <div className="footer">
+      {/* <img src="https://mitendicotthouse.org/wp-content/uploads/2016/01/whiskey.gif"/> */}
+      <img src="https://i.pinimg.com/originals/95/07/f2/9507f2f0a69e9a1795a2a08ebaa7fda7.gif" alt="gif" />
+    </div>
+  );
 }
 
 export default Footer;
